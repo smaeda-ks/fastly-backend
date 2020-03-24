d99419b5cf835b8fa78431b60c3d012f55f09228
Shohei Maeda <irt_m.jrsyo@ntworkers.com>
2020-03-24 16:40:59 +0900

    Support pool update

--- src/index.js
+++ src/index.js
@@ -26,27 +26,51 @@ const fastly = require('./fastly-promises');
   
           const active = (await service.getActiveVersion()).data;
           const backends = await service.readBackends(active.number);
-          const affected = backends.data.filter(config.affected);
+          const pools = await service.readPools(active.number);
+          const affectedBackends = backends.data.filter(config.affected);
+          const affectedPools = pools.data.filter(config.affected);
           
-          if (!affected.length) return;
-  
-          console.log(`Updating serivce: ${id}`);
+          let clone = null;
+          let isPoolsUpdated = false;
+          let isBackendsUpdated = false;
+          if (affectedBackends.length || affectedPools.length) {
+            clone = await service.cloneVersion(active.number);
+          }
+
+          if (affectedPools.length) {
+            //versionless objects are shouldn't be updated concurrently
+            //ref.NEX-1933
+            for (let pool of affectedPools) {
+              await service.updatePool(clone.data.number, pool.name, config.body)
+                .then(isPoolsUpdated = true)
+                .catch(err => {
+                  console.log(`Error (id: ${id}, pool: ${pool.name}): ${err.message}`);
+                  return
+                });
+            }
+            if (isPoolsUpdated) console.log(`Updated service (pools): ${id}, version: ${clone.data.number}`);
+          }
+
+          if (affectedBackends.length) {
+            // const clone = await service.cloneVersion(active.number);
+            await Promise.all(affectedBackends.map(backend => {
+              service.updateBackend(clone.data.number, backend.name, config.body)
+                .then(isBackendsUpdated = true)
+                .catch(err => {
+                  console.log(`Error (id: ${id}, backend: ${backend.name}): ${err.message}`);
+                  return
+              });
+            }));
+            if (isBackendsUpdated) console.log(`Updated service (backends): ${id}, version: ${clone.data.number}`);
+          }
           
-          const clone = await service.cloneVersion(active.number);
-          await Promise.all(affected.map(backend => {
-            service.updateBackend(clone.data.number, backend.name, config.body)
-              .catch(err => {
-                console.log(`Error (id: ${id}): ${err.message}`);
-                return
-            });
-          }));
-          await service.activateVersion(clone.data.number);
-          
-          console.log(`Updated service: ${id}, version: ${clone.data.number}`);
-          
+          if (clone !== null && (isPoolsUpdated || isBackendsUpdated)) {
+            await service.activateVersion(clone.data.number);
+          }
+
           await sleep(config.delay);
         } catch (err) {
-        console.log(`Error (id: ${id}) ${err.message}`);
+        console.log(`Error (id: ${id}, needs retry): ${err.message}`);
         }
       }));
       promiseIndex += maxConcurrentSize;
